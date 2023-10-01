d5d799293f4283fa64b968d46493b744bbd09df9
Frank Nguyen <vietnguy@student.42singapore.sg>
2023-10-01 15:33:19 +0800

    Solve get_next_line

---
+++ cursus/get_next_line/get_next_line.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vietnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 14:45:28 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/10/01 15:30:52 by vietnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*readwbuff(int fd, char *line)
+{
+	char	*buff;
+	ssize_t	nbytes;
+
+	buff = (char *)malloc((BUFFER_SIZE + 1));
+	if (buff == NULL)
+		return (line);
+	while (ft_strchr(line, '\n') == NULL)
+	{
+		nbytes = read(fd, buff, BUFFER_SIZE);
+		if (nbytes <= 0)
+			break ;
+		buff[nbytes] = '\0';
+		line = ft_strjoin(line, buff);
+	}
+	free(buff);
+	return (line);
+}
+
+char	*new_line(char *line)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+	{
+		free(line);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(line) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (line[i])
+		str[j++] = line[i++];
+	str[j] = '\0';
+	free(line);
+	return (str);
+}
+
+char	*ft_get_next_line(char *line)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	str = (char *)malloc(i + 2);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = readwbuff(fd, line);
+	if (!line)
+		return (NULL);
+	next_line = ft_get_next_line(line);
+	line = new_line(line);
+	return (next_line);
+}
+
+int	main(void)
+{
+	printf("%s", get_next_line(1));
+}
---
+++ cursus/get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vietnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 15:31:08 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/10/01 15:31:10 by vietnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
