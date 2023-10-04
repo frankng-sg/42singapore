e6daf93ea9abddfdd2e69a9a1eb879e648d1d43e
Frank Nguyen <frank.nguyen.vd@gmail.com>
2023-10-04 00:58:02 +0000

    Include ft_printf in the header file

--- cursus/ft_printf/ft_printf.h
+++ cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vietnguy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 12:30:36 by vietnguy          #+#    #+#             */
-/*   Updated: 2023/10/01 14:38:32 by vietnguy         ###   ########.fr       */
+/*   Updated: 2023/10/04 00:57:08 by vietnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int	ft_putstr(char *str);
 int	ft_puthex(size_t num, int isupper);
 int	ft_putptr(uintptr_t ptr);
 int	ft_putnbr(long num);
+int	ft_printf(const char *fmt, ...);
 
 #endif
