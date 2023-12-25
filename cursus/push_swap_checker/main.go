f183ca431666500e12c478d4c503ccda20203cdd
educative <educative@educative.io>
2023-12-25 18:14:29 +0800

    add special tests to check all combination of 5 numbers

--- cursus/push_swap_checker/main.go
+++ cursus/push_swap_checker/main.go
@@ -1,172 +1,221 @@
 package main
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
-	"github.com/fatih/color"
-	"log"
-	"os"
-	"os/exec"
-	"push_swap_checker/pkg/asciiart"
-	"push_swap_checker/pkg/exam"
-	"push_swap_checker/pkg/judge"
-	"strconv"
-	"strings"
-	"time"
+    "bytes"
+    "errors"
+    "fmt"
+    "github.com/fatih/color"
+    "log"
+    "os"
+    "os/exec"
+    "push_swap_checker/pkg/asciiart"
+    "push_swap_checker/pkg/exam"
+    "push_swap_checker/pkg/judge"
+    "strconv"
+    "strings"
+    "time"
 )
 
 func runCmd(command string, timeout time.Duration) (string, string, bool) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
 
-	cmd := exec.Command("bash", "-c", command)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
+    cmd := exec.Command("bash", "-c", command)
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
 
-	goRunCmd := func() <-chan struct{} {
-		done := make(chan struct{})
-		go func() {
-			defer close(done)
-			cmd.Run()
-		}()
-		return done
-	}
+    goRunCmd := func() <-chan struct{} {
+        done := make(chan struct{})
+        go func() {
+            defer close(done)
+            cmd.Run()
+        }()
+        return done
+    }
 
-	select {
-	case <-goRunCmd():
-		return stdout.String(), stderr.String(), false
-	case <-time.After(timeout):
-		cmd.Process.Kill()
-		return "", "", true
-	}
+    select {
+    case <-goRunCmd():
+        return stdout.String(), stderr.String(), false
+    case <-time.After(timeout):
+        cmd.Process.Kill()
+        return "", "", true
+    }
 }
 
 func extractSolution(data string) []string {
-	if len(data) <= 0 {
-		return []string{}
-	}
-	if data[len(data)-1] == '\n' {
-		data = data[:len(data)-1]
-	}
-	return strings.Split(data, "\n")
+    if len(data) <= 0 {
+        return []string{}
+    }
+    if data[len(data)-1] == '\n' {
+        data = data[:len(data)-1]
+    }
+    return strings.Split(data, "\n")
 }
 
 func toInts(data string) (nums []int) {
-	txtNums := strings.Split(data, " ")
-	for _, txtNum := range txtNums {
-		if len(txtNum) <= 0 {
-			continue
-		}
-		num, err := strconv.Atoi(txtNum)
-		if err != nil {
-			log.Fatal(err)
-		}
-		nums = append(nums, num)
-	}
-	return
+    txtNums := strings.Split(data, " ")
+    for _, txtNum := range txtNums {
+        if len(txtNum) <= 0 {
+            continue
+        }
+        num, err := strconv.Atoi(txtNum)
+        if err != nil {
+            log.Fatal(err)
+        }
+        nums = append(nums, num)
+    }
+    return
 }
 
 func showOK(msg string) {
-	color.Green("[OK] " + msg)
+    color.Green("[OK] " + msg)
 }
 
 func showLeak() {
-	color.Magenta("[LEAK]")
+    color.Magenta("[LEAK]")
 }
 
 func showFail(msg string) {
-	color.Red("[FAIL] %s", msg)
+    color.Red("[FAIL] %s", msg)
 }
 
 func leakAndTimeoutDetected(command string, timeout time.Duration) (isLeaked bool, isTimeout bool) {
-	var stderr string
-	_, stderr, isTimeout = runCmd(command, timeout)
-	isLeaked = strings.Contains(stderr, "SUMMARY") && !strings.Contains(stderr, "All heap blocks were freed")
-	return
+    var stderr string
+    _, stderr, isTimeout = runCmd(command, timeout)
+    isLeaked = strings.Contains(stderr, "SUMMARY") && !strings.Contains(stderr, "All heap blocks were freed")
+    return
+}
+
+func genAllSeq5() []string {
+    chanSeq := make([]string, 0, 120)
+    used := make([]bool, 5)
+    var try func(int, string)
+    try = func(n int, seq string) {
+        if n == 5 {
+            chanSeq = append(chanSeq, seq)
+            return
+        }
+        for i := 0; i < 5; i++ {
+            if used[i] {
+                continue
+            }
+            used[i] = true
+            try(n+1, seq+fmt.Sprintf(" %d", i))
+            used[i] = false
+        }
+    }
+    try(0, "")
+    return chanSeq
+}
+
+func testAllSeq5() bool {
+    var TimeOut = 5 * time.Second
+
+    fmt.Printf("Special Test: Test all sequences of 5 numbers\n")
+    allSeq := genAllSeq5()
+    for _, seq := range allSeq {
+        command := fmt.Sprintf("./push_swap %s", seq)
+        stdout, _, _ := runCmd(command, TimeOut)
+        solution := extractSolution(stdout)
+        if !judge.VerifySolution(toInts(seq), solution) {
+            showFail("stack is not sorted, input: " + seq)
+            return false
+        }
+        if len(solution) > 12 {
+            showFail(
+                fmt.Sprintf("too many moves...(expect: 12, actual: %d)\ninput: %s", len(solution), seq),
+            )
+            return false
+        }
+    }
+    showOK("")
+    return true
 }
 
 func runTests(tests []exam.Test) {
-	if _, err := os.Stat("push_swap"); errors.Is(err, os.ErrNotExist) {
-		showFail("push_swap executable not found")
-		return
-	}
+    if _, err := os.Stat("push_swap"); errors.Is(err, os.ErrNotExist) {
+        showFail("push_swap executable not found")
+        return
+    }
 
-	var TimeOut = 5 * time.Second
-	cntPass, cntFail, cntLeak := 0, 0, 0
-	for i, t := range tests {
-		fmt.Printf("Test %d: %s\n", i+1, t.Desc)
-		if len(t.Args) < 70 {
-			fmt.Printf("Args: %s\n", t.Args)
-		}
-		command := fmt.Sprintf("./push_swap %s", t.Args)
-		isLeaked, isTimeout := leakAndTimeoutDetected(command, TimeOut)
-		if isLeaked {
-			cntLeak++
-			showLeak()
-			continue
-		}
-		if isTimeout {
-			cntFail++
-			showFail("timeout...probably infinite loop")
-			continue
-		}
-		stdout, stderr, _ := runCmd(command, TimeOut)
-		if t.ExpectError {
-			if strings.Contains(stdout, "Error\n") || strings.Contains(stderr, "Error\n") {
-				cntPass++
-				showOK("")
-			} else {
-				cntFail++
-				showFail("should display error")
-			}
-			continue
-		}
-		if !t.ExpectOutput {
-			if stdout != "" {
-				cntFail++
-				showFail("should display nothing")
-			} else {
-				cntPass++
-				showOK("")
-			}
-			continue
-		}
-		solution := extractSolution(stdout)
-		if !judge.VerifySolution(toInts(t.Args), solution) {
-			showFail("stack is not sorted")
-			cntFail++
-			continue
-		}
-		score := 0
-		for i, limit := range t.Limits {
-			if len(solution) <= limit {
-				score = i + 1
-			}
-		}
-		if score == 0 {
-			showFail(fmt.Sprintf("too many moves\nLimit: %d, Actual: %d", t.Limits[0], len(solution)))
-			cntFail++
-			continue
-		}
-		cntPass++
-		if len(t.Limits) == 1 {
-			showOK(fmt.Sprintf("Limit: %d, Actual: %d", t.Limits[0], len(solution)))
-		} else {
-			showOK(fmt.Sprintf("Score: %d / %d\nLimit: %d, Actual: %d", score, len(t.Limits), t.Limits[score-1], len(solution)))
-		}
-	}
-	fmt.Println()
-	fmt.Println("-------------------------")
-	fmt.Printf("SUMMARY: %s %s %s\n",
-		color.GreenString("%d [OK]", cntPass),
-		color.RedString("%d [Fail]", cntFail),
-		color.MagentaString("%d [LEAK]", cntLeak),
-	)
+    var TimeOut = 5 * time.Second
+    cntPass, cntFail, cntLeak := 0, 0, 0
+    if !testAllSeq5() {
+        cntFail++
+    }
+    for i, t := range tests {
+        fmt.Printf("Test %d: %s\n", i+1, t.Desc)
+        if len(t.Args) < 70 {
+            fmt.Printf("Args: %s\n", t.Args)
+        }
+        command := fmt.Sprintf("./push_swap %s", t.Args)
+        isLeaked, isTimeout := leakAndTimeoutDetected(command, TimeOut)
+        if isLeaked {
+            cntLeak++
+            showLeak()
+            continue
+        }
+        if isTimeout {
+            cntFail++
+            showFail("timeout...probably infinite loop")
+            continue
+        }
+        stdout, stderr, _ := runCmd(command, TimeOut)
+        if t.ExpectError {
+            if strings.Contains(stdout, "Error\n") || strings.Contains(stderr, "Error\n") {
+                cntPass++
+                showOK("")
+            } else {
+                cntFail++
+                showFail("should display error")
+            }
+            continue
+        }
+        if !t.ExpectOutput {
+            if stdout != "" {
+                cntFail++
+                showFail("should display nothing")
+            } else {
+                cntPass++
+                showOK("")
+            }
+            continue
+        }
+        solution := extractSolution(stdout)
+        if !judge.VerifySolution(toInts(t.Args), solution) {
+            showFail("stack is not sorted")
+            cntFail++
+            continue
+        }
+        score := 0
+        for i, limit := range t.Limits {
+            if len(solution) <= limit {
+                score = i + 1
+            }
+        }
+        if score == 0 {
+            showFail(fmt.Sprintf("too many moves\nLimit: %d, Actual: %d", t.Limits[0], len(solution)))
+            cntFail++
+            continue
+        }
+        cntPass++
+        if len(t.Limits) == 1 {
+            showOK(fmt.Sprintf("Limit: %d, Actual: %d", t.Limits[0], len(solution)))
+        } else {
+            showOK(fmt.Sprintf("Score: %d / %d\nLimit: %d, Actual: %d", score, len(t.Limits), t.Limits[score-1], len(solution)))
+        }
+    }
+    fmt.Println()
+    fmt.Println("-------------------------")
+    fmt.Printf("SUMMARY: %s %s %s\n",
+        color.GreenString("%d [OK]", cntPass),
+        color.RedString("%d [Fail]", cntFail),
+        color.MagentaString("%d [LEAK]", cntLeak),
+    )
 }
 
 func main() {
-	asciiart.DisplayPushSwapLogo()
-	var tests = exam.NewTestSuite()
-	runTests(tests)
+    asciiart.DisplayPushSwapLogo()
+    var tests = exam.NewTestSuite()
+    runTests(tests)
 }
