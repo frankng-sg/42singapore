e8b460c1e4bd5e3ed01f42ee491665d5751f2c22
Frank Nguyen <vietnguy@student.42singapore.sg>
2023-12-28 13:15:06 +0800

    Display 600x400 windows

--- cursus/FdF/src/main.c
+++ cursus/FdF/src/main.c
@@ -6,16 +6,20 @@
 /*   By: vietnguy <vietnguy@42mail.sutd.edu.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 13:51:52 by vietnguy          #+#    #+#             */
-/*   Updated: 2023/12/28 13:05:35 by vietnguy         ###   ########.fr       */
+/*   Updated: 2023/12/28 13:14:31 by vietnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pkg/libft/libft.h"
 #include "../pkg/gnl/gnl.h"
 #include "../pkg/mlx_linux/mlx.h"
-#include <stdio.h>
 
 int	main()
 {
-	printf("Hello World\n");
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 600, 400, "Hello World!");
+	mlx_loop(mlx);
 }
